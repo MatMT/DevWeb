45ac526dfa14b6bf410a696c307f8c15e9f19a97
MatMT <oscarmateoelias@gmail.com>
2022-11-12 00:55:39 -0600

    Paginador con Js

--- AppSalon-PHP-MVC-JS-SASS/src/js/app.js
+++ AppSalon-PHP-MVC-JS-SASS/src/js/app.js
@@ -1,4 +1,6 @@
 let paso = 1;
+const pasoInicial = 1;
+const pasoFinal = 3;
 
 // Cargado todo el DOM ejecutar la siguiente función
 document.addEventListener('DOMContentLoaded', function () {
@@ -6,7 +8,11 @@ document.addEventListener('DOMContentLoaded', function () {
 });
 
 function iniciarApp() {
+    mostrarSeccion(); // Muestra y oculta las secciones
     tabs(); // Cambia la sección cuando se presiona los tabs
+    botonesPaginador(); // Agrega o quita los botones del paginador
+    paginaSiguiente();
+    paginaAnterior();
 }
 
 function mostrarSeccion() {
@@ -15,11 +21,22 @@ function mostrarSeccion() {
     if (seccionAnterior) {
         seccionAnterior.classList.remove('mostrar');
     }
+
     // Seleccionar la sección con el paso...
     const pasoSelector = `#paso-${paso}`;
     const seccion = document.querySelector(pasoSelector);
     seccion.classList.add('mostrar');
     console.log(seccion);
+
+    // Quita la clase .actual al tab anterior
+    const tabAnterior = document.querySelector('.actual');
+    if (tabAnterior) {
+        tabAnterior.classList.remove('actual');
+    }
+
+    // Resalta el tab actual
+    const tab = document.querySelector(`[data-paso="${paso}"]`);
+    tab.classList.add('actual');
 }
 
 function tabs() {
@@ -28,6 +45,43 @@ function tabs() {
         boton.addEventListener('click', function (e) {
             paso = parseInt(e.target.dataset.paso);
             mostrarSeccion();
+            botonesPaginador()
         });
+    });
+}
+
+function botonesPaginador() {
+    const paginaAnterior = document.querySelector('#anterior');
+    const paginaSiguiente = document.querySelector('#siguiente');
+
+    if (paso === 1) {
+        paginaAnterior.classList.add('ocultar');
+        paginaSiguiente.classList.remove('ocultar');
+    } else if (paso === 3) {
+        paginaAnterior.classList.remove('ocultar');
+        paginaSiguiente.classList.add('ocultar');
+    } else {
+        paginaAnterior.classList.remove('ocultar');
+        paginaSiguiente.classList.remove('ocultar');
+    }
+
+    mostrarSeccion();
+}
+
+function paginaAnterior() {
+    const paginaAnterior = document.querySelector('#anterior');
+    paginaAnterior.addEventListener('click', function () {
+        if (paso <= pasoInicial) return; // Corta el código
+        paso--;
+        botonesPaginador();
+    });
+}
+
+function paginaSiguiente() {
+    const paginaSiguiente = document.querySelector('#siguiente');
+    paginaSiguiente.addEventListener('click', function () {
+        if (paso >= pasoFinal) return; // Corta el código
+        paso++;
+        botonesPaginador();
     });
 }
