aad8166bbefe36991ac95631e1279f87f0ec9959
Mateo Elías <oscarmateoelias@gmail.com>
2022-12-15 15:37:13 -0600

    Modelo de Usuario

---
+++ UpTask_MVC/models/Usuario.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Model;
+
+class Usuario extends ActiveRecord
+{
+    protected static $tabla = 'usuarios';
+    protected static $columnasDB = ['id', 'email', 'password', 'token', 'confirmado'];
+
+    public function __construct($args = [])
+    {
+        $this->id = $args['id'] ?? null;
+        $this->nombre = $args['nombre'] ?? '';
+        $this->email = $args['email'] ?? '';
+        $this->password = $args['password'] ?? '';
+        $this->token = $args['token'] ?? '';
+        $this->confirmado = $args['confirmado'] ?? '';
+    }
+}
