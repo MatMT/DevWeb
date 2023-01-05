d7a890e57996d031aaa9d1914896b6cefa23ad24
Mateo Elías <oscarmateoelias@gmail.com>
2023-01-04 19:31:17 -0600

    Validación - Alerta | Nombre tarea obligatorio

--- UpTask_MVC/src/js/tareas.js
+++ UpTask_MVC/src/js/tareas.js
@@ -43,8 +43,55 @@
                     modal.remove();
                 }, 600);
             }
+
+            if (e.target.classList.contains('submit-nueva-tarea')) {
+                submitFormularioNuevaTarea();
+            }
         })
 
-        document.querySelector('body').appendChild(modal);
+        document.querySelector('.dashboard').appendChild(modal);
     }
+
+    function submitFormularioNuevaTarea() {
+        // Extraer value
+        const tarea = document.querySelector('#tarea').value.trim();
+
+        if (!tarea) {
+            // Tarea vacía
+            mostrarAlerta('El Nombre de la tarea es Obligatorio', 'error', document.querySelector('.formulario legend'));
+            return;
+        }
+
+        agregarTarea(tarea);
+    };
+
+    // Muestra un mensaje en la interfaz
+    function mostrarAlerta(mensaje, tipo, referencia) {
+        // Previene la creación de multiples alertas
+        const alertaPrevia = document.querySelector('.alerta');
+        if (alertaPrevia) {
+            alertaPrevia.remove();
+        }
+
+        const alerta = document.createElement('DIV');
+        // Agregamos la clase de la alerta
+        alerta.classList.add('alerta', tipo)
+
+        // Agregamos el mensaje
+        alerta.textContent = mensaje;
+
+        // Insertar antes del legend
+        referencia.parentElement.insertBefore(alerta, referencia.nextElementSibling);
+
+        // Eliminar la alerta después de 5 segundos
+        setTimeout(() => {
+            alerta.remove();
+        }, 4500);
+    };
+
+    // Consultar el servidor para agregar la tarea
+    function agregarTarea(tarea) {
+        console.log('añadiendo');
+    }
+
 })();
