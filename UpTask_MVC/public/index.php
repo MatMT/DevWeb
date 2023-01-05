91c978005763b76b8862d0068676d309920aa227
Mateo Elías <oscarmateoelias@gmail.com>
2023-01-05 10:15:51 -0600

    Routing y API de Tareas 1/2

--- UpTask_MVC/public/index.php
+++ UpTask_MVC/public/index.php
@@ -4,6 +4,7 @@ require_once __DIR__ . '/../includes/app.php';
 
 use Controllers\LoginController;
 use Controllers\DashboardController;
+use Controllers\TareaController;
 use MVC\Router;
 
 $router = new Router();
@@ -36,6 +37,11 @@ $router->post('/crear-proyecto', [DashboardController::class, 'crear_proyecto'])
 $router->get('/proyecto', [DashboardController::class, 'proyecto']);
 $router->get('/perfil', [DashboardController::class, 'perfil']);
 
+// API para las tareas
+$router->get('/api/tareas', [TareaController::class, 'index']);
+$router->post('/api/tarea', [TareaController::class, 'crear']);
+$router->post('/api/tareas/actualizar', [TareaController::class, 'update']);
+$router->post('/api/tareas/eliminar', [TareaController::class, 'delete']);
 
 // Comprueba y valida las rutas, que existan y les asigna las funciones del Controlador
 $router->comprobarRutas();
