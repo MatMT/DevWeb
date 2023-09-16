8a8a44a685a917e88223cbae4ca4707689691ed7
Mateo Elias <oscarmateoelias@gmail.com>
2023-09-16 17:54:37 -0600

    The return...

--- UpTask_MVC/src/js/app.js
+++ UpTask_MVC/src/js/app.js
@@ -1,4 +1,5 @@
 const mobileMenuBtn = document.querySelector('#mobile-menu');
+const cerrarMenuBtn = document.querySelector('#cerrar-menu');
 const sidebar = document.querySelector('.sidebar');
 
 if (mobileMenuBtn) {
@@ -6,3 +7,21 @@ if (mobileMenuBtn) {
         sidebar.classList.toggle('mostrar');
     });
 }
+
+if (cerrarMenuBtn) {
+    cerrarMenuBtn.addEventListener('click', function () {
+        sidebar.classList.add('ocultar');
+        setTimeout(() => {
+            sidebar.classList.remove('mostrar');
+            sidebar.classList.remove('ocultar');
+        }, 1000);
+    })
+}
+
+// Elimina la clase de mostrar, en tamaño de talet y mayores
+const anchoScreen = document.body.clientWidth;
+window.addEventListener('resize', function () {
+    if (anchoScreen >= 768) {
+        sidebar.classList.remove('mostrar');
+    }
+})
--- UpTask_MVC/views/templates/sidebar.php
+++ UpTask_MVC/views/templates/sidebar.php
@@ -1,8 +1,19 @@
 <aside class="sidebar">
-    <h2>UpTask</h2>
+
+    <div class="contenedor-sidebar">
+        <h2>UpTask</h2>
+        <div class="cerrar-menu">
+            <img src="build/img/cerrar.svg" alt="imagen cerrar menu" id="cerrar-menu">
+        </div>
+    </div>
+
     <nav class="sidebar-nav">
         <a href="/dashboard" class="<?php echo ($titulo === 'Proyectos') ? 'activo' : ''; ?>">Proyectos</a>
         <a href="/crear-proyecto" class="<?php echo ($titulo === 'Crear Proyecto') ? 'activo' : '';  ?>">Crear Proyectos</a>
         <a href="/perfil" class="<?php echo ($titulo === 'Perfil') ? 'activo' : '';  ?>">Perfil</a>
     </nav>
+
+    <div class="cerrar-session-mobile">
+        <a href="/logout" class="cerrar-sesion">Cerrar Sesión</a>
+    </div>
 </aside>
