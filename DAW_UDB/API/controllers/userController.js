cfa250f90fba62cf824219bb4a191004f6011e51
Mateo Elias <oscarmateoelias@gmail.com>
2023-10-04 00:05:23 -0600

    Registro y validación de usuario

--- DAW_UDB/API/controllers/userController.js
+++ DAW_UDB/API/controllers/userController.js
@@ -1,40 +1,45 @@
+import { validationResult } from "express-validator";
+import User from "../models/User.js";
+import bcrypt from 'bcrypt';
+
 const formularioLogin = (req, res) => {
     res.render('auth/login', {
     })
 }
 
-const formularioRegistro = (req, res) => {
-    res.render('auth/registro', {
-    })
+const formularioRegistro = async (req, res) => {
+    // Mensajes de error de Express Validator
+    const errores = validationResult(req);
+    if (!errores.isEmpty()) {
+        return res.status(400).json({ errores: errores.array() });
+    }
+
+    // Verificar si el usuario ya esta registrado
+    const { carnet } = req.body;
+    const { password } = req.body;
+
+    let existUser = await User.findOne({ carnet });
+    if (existUser) {
+        return res.status(400).json({ msg: 'El usuario ya esta registrado' });
+    }
+
+    // Encaje de datos según el modelo
+    const usuario = new User(req.body);
+
+    // Hasear el password
+    usuario.password = await bcrypt.hash(password, 10)
+
+    try {
+        await usuario.save();
+        res.json({ msg: 'Usuario Creado Correctamente' });
+    } catch (error) {
+        console.log(error);
+    }
+
+    res.end();
 }
 
 export {
     formularioLogin,
     formularioRegistro
 }
-
-/*
-
-Aquí te proporcionaré ejemplos de endpoints que podrías implementar en tu API para gestionar la información de los pensum de las carreras de Técnico en Ingeniería en Computación y de Ingeniería en Computación de la UDB, siguiendo los requisitos mencionados:
-
-Consulta de Prerrequisitos de una Materia por Código:
-
-Método: GET
-Endpoint: /api/prerrequisitos/:codigo
-Descripción: Permite a los usuarios consultar los prerrequisitos de una materia específica identificada por su código.
-Consulta de Materias por Ciclo:
-
-Método: GET
-Endpoint: /api/materias/:carrera/:ciclo
-Descripción: Permite a los usuarios consultar las materias disponibles en un ciclo académico específico para una carrera determinada.
-Inscripción de Materias:
-
-Método: POST
-Endpoint: /api/inscripcion/:carrera
-Descripción: Permite a los usuarios inscribirse en materias de una carrera seleccionada. Los datos de las materias a inscribir se envían en el cuerpo de la solicitud.
-Eliminación de Inscripciones de Materias:
-
-Método: DELETE
-Endpoint: /api/inscripcion/:id
-Descripción: Permite a los usuarios eliminar sus inscripciones de materias. El id podría ser el identificador único de la inscripción.
-*/
--- DAW_UDB/API/index.js
+++ DAW_UDB/API/index.js
@@ -1,5 +1,5 @@
 import express from 'express';
-// import userRoutes from './routes/userRoutes.js';
+import userRoutes from './routes/userRoutes.js';
 import conectarDB from './config/db.js';
 
 // Crear la app =====================
@@ -8,12 +8,17 @@ const server = express();
 // Conectar a la base de datos
 conectarDB();
 
-// Routing ==========================
-// get = ruta especifica | use = rutas con una diagonal
-// server.use('/', userRoutes);
-
 // Definimos un puerto para arrancar el servidor
 const port = process.env.PORT || 4000;
+
+// Habilitar lectura de valores
+server.use(express.json());
+
+// Routing ==========================
+// get = ruta especifica | use = rutas con una diagonal
+server.use('/api/usuarios', userRoutes);
+
+// Arrancar el servidor
 server.listen(port, '0.0.0.0', () => {
     console.log(`El servidor esta funcionando en el puerto ${port}`);
 });
