309d949e97e559a44aa50f06f728b572c12fa4a6
Mateo Elias <oscarmateoelias@gmail.com>
2023-10-05 00:14:07 -0600

    Middleware Auth

--- DAW_UDB/API/controllers/authController.js
+++ DAW_UDB/API/controllers/authController.js
@@ -1,8 +1,13 @@
 import User from "../models/User.js";
 import bcrypt from 'bcrypt';
+import { validationResult } from "express-validator";
 
 const authUser = async (req, res, next) => {
-    // Buscar si hay errores
+    // Mensajes de error de Express Validator
+    const errores = validationResult(req);
+    if (!errores.isEmpty()) {
+        return res.status(400).json({ errores: errores.array() });
+    }
 
     // Buscar el usuario par aver si esta registrado
     const { carnet, password } = req.body;
@@ -16,16 +21,24 @@ const authUser = async (req, res, next) => {
     // Verificar el password y autenticar el usuario
     if (bcrypt.compareSync(password, usuario.password)) {
         // Generar JWT
+        const token = jwt.sign({
+            id: usuario._id,
+            nombre: usuario.nombre,
+            carnet: usuario.carnet,
+        }, process.env.SECRET, {
+            expiresIn: '8h'
+        });
+
+        res.json({ token });
     } else {
         res.status(401).json({ msg: "Password Incorrecto" });
     }
 
-
     res.end();
 }
 
-const authenticatedUser = (req, res) => {
-
+const authenticatedUser = (req, res, next) => {
+    res.json({ usuario: req.usuario });
 }
 
 export {
--- DAW_UDB/API/routes/authRoutes.js
+++ DAW_UDB/API/routes/authRoutes.js
@@ -1,11 +1,20 @@
 import express from 'express';
+import auth from '../middleware/auth.js';
 import { check } from 'express-validator';
 import { authUser, authenticatedUser } from '../controllers/authController.js';
 
 const router = express.Router();
 
-router.post('/', authUser);
+router.post('/',
+    [
+        check('carnet', 'Agrega un carnet válido').isString(),
+        check('password', 'El Password es obligatorio').notEmpty()
+    ]
+    , authUser);
 
-router.get('/', authenticatedUser);
+router.get('/',
+    auth,
+    authenticatedUser
+);
 
 export default router;
