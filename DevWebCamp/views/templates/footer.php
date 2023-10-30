53073387be6592199817e62b88cc80242f46a117
Mateo Elias <oscarmateoelias@gmail.com>
2023-10-30 11:04:11 -0600

    Header & Footer finalizado

---
+++ DevWebCamp/views/templates/footer.php
@@ -0,0 +1,41 @@
+<footer class="footer">
+    <div class="footer__grid">
+        <div class="footer__contenido">
+            <h3 class="footer__logo">
+                &#60;DevWebCamp />
+            </h3>
+
+            <p class="footer__texto">
+                DevWebCamp es una conferencia para Dev's de todos los niveles, se lleva a cabo de manera presencial y en línea.
+            </p>
+        </div>
+
+        <nav class="menu-redes">
+            <a class="menu-redes__enlace" rel="noopener noreferrer" target="_blank" href="https://facebook.com/C%C3%B3digo-Con-Juan-103341632130628">
+                <span class="menu-redes__ocultar">Facebook</span>
+            </a>
+            <a class="menu-redes__enlace" rel="noopener noreferrer" target="_blank" href="https://twitter.com/codigoconjuan">
+                <span class="menu-redes__ocultar">Twitter</span>
+            </a>
+            <a class="menu-redes__enlace" rel="noopener noreferrer" target="_blank" href="https://youtube.com/codigoconjuan">
+                <span class="menu-redes__ocultar">YouTube</span>
+            </a>
+            <a class="menu-redes__enlace" rel="noopener noreferrer" target="_blank" href="https://instagram.com/codigoconjuan">
+                <span class="menu-redes__ocultar">Instagram</span>
+            </a>
+            <a class="menu-redes__enlace" rel="noopener noreferrer" target="_blank" href="https://tiktok.com/@codigoconjuan">
+                <span class="menu-redes__ocultar">Tiktok</span>
+            </a>
+            <a class="menu-redes__enlace" rel="noopener noreferrer" target="_blank" href="https://github.com/codigoconjuan">
+                <span class="menu-redes__ocultar">GitHub</span>
+            </a>
+        </nav>
+    </div>
+
+    <p class="footer__copyright">
+        DevWebCamp
+        <span class="footer__copyright--regular">
+            - Todos los derechos reservados <?php echo date('Y'); ?>
+        </span>
+    </p>
+</footer>
--- DevWebCamp/views/templates/header.php
+++ DevWebCamp/views/templates/header.php
@@ -21,3 +21,19 @@
         </div>
     </div>
 </header>
+
+<div class="barra">
+    <div class="barra__contenido">
+        <a href="/">
+            <h2 class="barra__logo">
+                &#60;DevWebCamp />
+            </h2>
+        </a>
+        <nav class="navegacion">
+            <a href="/devwebcamp" class="navegacion__enlace">Evento</a>
+            <a href="/paquetes" class="navegacion__enlace">Paquetes</a>
+            <a href="/workshops" class="navegacion__enlace">Workshops / Conferencias</a>
+            <a href="/registro" class="navegacion__enlace">Comprar Pase</a>
+        </nav>
+    </div>
+</div>
