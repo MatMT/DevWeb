3c226b09aad13d40464345a8ee370c196da53dbb
Mateo Elias <oscarmateoelias@gmail.com>
2023-11-18 00:14:00 -0600

    Listando registrados en panel ADMIN

--- DevWebCamp/controllers/RegistradosController.php
+++ DevWebCamp/controllers/RegistradosController.php
@@ -3,13 +3,50 @@
 namespace Controllers;
 
 use MVC\Router;
+use Classes\Paginacion;
+use Model\Paquete;
+use Model\RegistroPaquetes;
+use Model\Usuario;
 
 class RegistradosController
 {
     public static function index(Router $router)
     {
+        if (!is_admin()) {
+            header('Location: /login');
+        }
+
+        // PAGINACIÓN =================================
+        $pagina_actual = $_GET['page'];
+        $pagina_actual = filter_var($pagina_actual, FILTER_VALIDATE_INT);
+
+        // Validación de URL
+        if (!$pagina_actual || $pagina_actual < 1) {
+            header('Location: /admin/registrados?page=1');
+        }
+
+        $registros_por_pagina = 20;
+        $total = RegistroPaquetes::total();
+        $paginacion = new Paginacion($pagina_actual, $registros_por_pagina, $total);
+
+        if ($paginacion->total_paginas() < $pagina_actual) {
+            header('Location: /admin/registrados?page=1');
+        }
+
+        $registros = RegistroPaquetes::paginar($registros_por_pagina, $paginacion->offset());
+
+        // Cruzar información
+        foreach ($registros as $registro) {
+            $registro->usuario = Usuario::find($registro->usuario_id);
+            $registro->paquete = Paquete::find($registro->paquete_id);
+        }
+
+        // PAGINACIÓN =================================
+
         $router->render('admin/registrados/index', [
-            'titulo' => 'Usuariso registrados'
+            'titulo' => 'Usuariso registrados',
+            'registros' => $registros,
+            'paginacion' => $paginacion->paginacion()
         ]);
     }
 }
--- DevWebCamp/views/admin/registrados/index.php
+++ DevWebCamp/views/admin/registrados/index.php
@@ -1,1 +1,44 @@
 <h2 class="dashboard__heading"><?php echo $titulo; ?></h1>
+
+    <div class="dashboard__contenedor">
+        <?php if (!empty($registros)) { ?>
+            <table class="table">
+                <thead class="table__thead">
+                    <tr>
+                        <th scope="col" class="table__th">Nombre</th>
+                        <th scope="col" class="table__th">Email</th>
+                        <th scope="col" class="table__th">Plan</th>
+                        <th scope="col" class="table__th"></th>
+                    </tr>
+                </thead>
+
+                <tbody class="table__tbody">
+                    <?php foreach ($registros as $registro) { ?>
+                        <tr class="table__tr">
+
+                            <td class="table__td">
+                                <?php echo $registro->usuario->nombre . " " . $registro->usuario->apellido; ?>
+                            </td>
+
+                            <td class="table__td">
+                                <?php echo $registro->usuario->email; ?>
+                            </td>
+
+                            <td class="table__td">
+                                <?php echo $registro->paquete->nombre; ?>
+                            </td>
+                        </tr>
+                    <?php  }; ?>
+                </tbody>
+
+            </table>
+
+        <?php } else { ?>
+            <p class="text-center">No hay Registros registrados</p>
+        <?php };  ?>
+
+    </div>
+
+    <?php
+    echo $paginacion;
+    ?>
