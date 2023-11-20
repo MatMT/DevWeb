833340e21b505242d32d3e81b694428fe3ac2046
Mateo Elias <oscarmateoelias@gmail.com>
2023-11-20 13:58:08 -0600

    Removing project

--- Pelucos-PHP-MVC-JS-SASS/views/auth/confirmar-cuenta.php
+++
@@ -1,7 +0,0 @@
-<h1 class="nombre-pagina">Confirmar Cuenta</h1>
-
-<?php include_once __DIR__ . '/../templates/alertas.php' ?>
-
-<div class="acciones">
-    <a href="/">Inciar sesión</a>
-</div>
--- Pelucos-PHP-MVC-JS-SASS/views/auth/mensaje.php
+++
@@ -1,5 +0,0 @@
-<h1 class="nombre-pagina">Confirma tu cuenta</h1>
-
-<p class="descripcion-pagina">
-    Hemos enviado las instrucciones para confirmar tu cuenta a tu e-mail.
-</p>
